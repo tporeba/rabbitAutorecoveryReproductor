47d30554ce59c85e8883f2c4c6d1b7004dd8fb81
Tomasz Poreba <tomasz.poreba@siemens.com>
2022-09-08 13:52:53 +0200

    script for testing in a loop

---
+++ docker/testingScript.sh
@@ -0,0 +1,36 @@
+#~/bin/bash
+# usage: ./testingScript.sh <numberOfIterations>
+
+function waitForRabbitUp {
+    local rabbitGuiUrl=$1
+    while ! curl -s -o /dev/null $rabbitGuiUrl
+    do
+        echo waiting for rabbit to show GUI on $rabbitGuiUrl
+        sleep 2
+    done
+}
+
+sudo docker-compose up -d
+sleep 10
+waitForRabbitUp "http://localhost:15671/#/"
+waitForRabbitUp "http://localhost:15672/#/"
+
+count=${1:-10}
+delay=20
+echo "Will do $count test iterations"
+
+for i in $(seq $count); do
+    echo "Iteration $i"
+
+    sudo docker-compose stop rabbit2
+    sleep $delay
+    sudo docker-compose up -d
+    waitForRabbitUp "http://localhost:15672/#/"
+
+    sudo docker-compose stop rabbit1
+    sleep $delay
+    sudo docker-compose up -d
+    waitForRabbitUp "http://localhost:15671/#/"
+done
+
+echo "Finishing"
