f77bd7184dfc61e262e966f6d253ba7d8f22bcb3
Tomasz Poreba <tomasz.poreba@siemens.com>
2022-09-13 14:47:10 +0200

    better predicate that works also in version 5.7.3 when exception is not wrapped in IOException

--- app/src/main/java/com/example/rabbitautorecoveryreproductor/AmqpConnectionFactory.java
+++ app/src/main/java/com/example/rabbitautorecoveryreproductor/AmqpConnectionFactory.java
@@ -3,13 +3,17 @@ package com.example.rabbitautorecoveryreproductor;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import com.google.common.base.Splitter;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Address;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ShutdownSignalException;
+import com.rabbitmq.client.impl.recovery.RecordedEntity;
 import com.rabbitmq.client.impl.recovery.RetryHandler;
 import com.rabbitmq.client.impl.recovery.TopologyRecoveryRetryHandlerBuilder;
 import com.rabbitmq.client.impl.recovery.TopologyRecoveryRetryLogic;
@@ -21,6 +25,24 @@ public class AmqpConnectionFactory {
     private static final Logger log = LogManager.getLogger();
     protected static final int TOPOLOGY_RECOVER_RETRY_INITIAL_DELAY_MS = 5000;
     protected static final int TOPOLOGY_RECOVER_RETRY_ATTEMPTS = 5;
+
+    /**
+     * The original predicate from TopologyRecoveryRetryLogic.CHANNEL_CLOSED_NOT_FOUND is working ok for version 5.15.0
+     * but on older versions (5.7.3 or 4.12.0) it returns false, because exception intercepted by the handler
+     * is not wrapped in additional IOException.
+     *
+     * This predicate accepts this alternative scenario.
+     */
+    private static final BiPredicate<RecordedEntity, Exception> CHANNEL_CLOSED_NOT_FOUND_WITHOUT_IOEXCEPTION = (entity, ex) -> {
+        if (ex instanceof ShutdownSignalException) {
+            ShutdownSignalException cause = (ShutdownSignalException) ex;
+            if (cause.getReason() instanceof AMQP.Channel.Close) {
+                return ((AMQP.Channel.Close) cause.getReason()).getReplyCode() == 404;
+            }
+        }
+        return false;
+    };
+
     private final String username;
     private final String password;
     private final List<Address> hosts;
@@ -60,6 +82,8 @@ public class AmqpConnectionFactory {
 
     private RetryHandler buildTopologyRecoveryRetryHandler(long initialDelayMs, int retryAttempts) {
         final TopologyRecoveryRetryHandlerBuilder builder = TopologyRecoveryRetryLogic.RETRY_ON_QUEUE_NOT_FOUND_RETRY_HANDLER;
+        builder.bindingRecoveryRetryCondition(TopologyRecoveryRetryLogic.CHANNEL_CLOSED_NOT_FOUND.or(CHANNEL_CLOSED_NOT_FOUND_WITHOUT_IOEXCEPTION));
+        builder.consumerRecoveryRetryCondition(TopologyRecoveryRetryLogic.CHANNEL_CLOSED_NOT_FOUND.or(CHANNEL_CLOSED_NOT_FOUND_WITHOUT_IOEXCEPTION));
         builder.backoffPolicy(new ExpBackoffPolicy(initialDelayMs, 2));
         builder.retryAttempts(retryAttempts);
         return builder.build();
