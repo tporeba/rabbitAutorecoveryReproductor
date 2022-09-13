1daac34c1c300f876dec2918fda7f1399f01633b
Tomasz Poreba <tomasz.poreba@siemens.com>
2022-09-13 14:25:28 +0200

    using TopologyRecoveryRetryHandler to workaround the problem

--- app/src/main/java/com/example/rabbitautorecoveryreproductor/AmqpConnectionFactory.java
+++ app/src/main/java/com/example/rabbitautorecoveryreproductor/AmqpConnectionFactory.java
@@ -10,12 +10,17 @@ import com.google.common.base.Splitter;
 import com.rabbitmq.client.Address;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.impl.recovery.RetryHandler;
+import com.rabbitmq.client.impl.recovery.TopologyRecoveryRetryHandlerBuilder;
+import com.rabbitmq.client.impl.recovery.TopologyRecoveryRetryLogic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AmqpConnectionFactory {
 
     private static final Logger log = LogManager.getLogger();
+    protected static final int TOPOLOGY_RECOVER_RETRY_INITIAL_DELAY_MS = 5000;
+    protected static final int TOPOLOGY_RECOVER_RETRY_ATTEMPTS = 5;
     private final String username;
     private final String password;
     private final List<Address> hosts;
@@ -30,12 +35,12 @@ public class AmqpConnectionFactory {
             log.debug("adding address host={}, port={}", host, port);
             return new Address(host, port);
         }).collect(Collectors.toList());
-
     }
 
     public Connection createNewConnection() throws RabbitMQConnectionException {
         try {
             ConnectionFactory factory = new ConnectionFactory();
+            factory.setTopologyRecoveryRetryHandler(buildTopologyRecoveryRetryHandler(TOPOLOGY_RECOVER_RETRY_INITIAL_DELAY_MS, TOPOLOGY_RECOVER_RETRY_ATTEMPTS));
             factory.setThreadFactory(new ThreadFactory() {
                 @Override
                 public Thread newThread(Runnable r) {
@@ -52,4 +57,12 @@ public class AmqpConnectionFactory {
             throw new RabbitMQConnectionException("Failed to start AMQP connection", e);
         }
     }
+
+    private RetryHandler buildTopologyRecoveryRetryHandler(long initialDelayMs, int retryAttempts) {
+        final TopologyRecoveryRetryHandlerBuilder builder = TopologyRecoveryRetryLogic.RETRY_ON_QUEUE_NOT_FOUND_RETRY_HANDLER;
+        builder.backoffPolicy(new ExpBackoffPolicy(initialDelayMs, 2));
+        builder.retryAttempts(retryAttempts);
+        return builder.build();
+    }
+
 }
